84a626f56ba7a3aeb51900fbccfd86a5a3076413
Tyagunova Daria <119135929+husden@users.noreply.github.com>
2026-03-03 21:19:08 +0300

    Add main.cpp with basic output

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Smart Home System starting..." << '\n';
+    std::cout << "Project by: Daria and [second participant]" << '\n';
+    
+    return 0;
+}
