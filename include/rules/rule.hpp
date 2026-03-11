de66ad7664ebf39fcef41add40540a08c8a01f78
Тягунова Дарья Ивановна <tyagunova_daria@MacBook-Air-Tagunova.local>
2026-03-11 10:00:12 +0300

    Add abstract Rule class

--- include/rules/rule.hpp
+++ include/rules/rule.hpp
@@ -1,1 +1,24 @@
+#ifndef RULE_HPP
+#define RULE_HPP
 
+#include <string>
+
+class Rule {
+protected:
+    std::string name;
+    bool isActive;
+
+public:
+    Rule(const std::string& ruleName) : name(ruleName), isActive(true) {}
+    virtual ~Rule() = default;
+
+    virtual bool checkCondition() = 0;
+    virtual void execute() = 0;
+
+    std::string getName() const { return name; }
+    void activate() { isActive = true; }
+    void deactivate() { isActive = false; }
+    bool isActiveRule() const { return isActive; }
+};
+
+#endif
