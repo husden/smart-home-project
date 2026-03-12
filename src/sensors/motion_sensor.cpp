ccb90aba058612a9e6c0044e4b50ba9ab93d811e
Sasha Doubik-Enman <sashadoubikenman@Sashas-MacBook-Air.local>
2026-03-12 20:50:01 +0300

    feat: add MotionSensor class implementation

--- src/sensors/motion_sensor.cpp
+++ src/sensors/motion_sensor.cpp
@@ -1,3 +1,16 @@
-//
-// Created by Sasha Doubik-Enman on 3/12/26.
-//
+#include "sensors/motion_sensor.hpp"
+
+MotionSensor::MotionSensor(const std::string& name)
+    : Sensor(name), motionDetected_(false) {}
+
+std::optional<double> MotionSensor::readValue() {
+    if (!isOperational()) {
+        return std::nullopt;
+    }
+    return motionDetected_ ? 1.0 : 0.0;
+}
+
+std::string MotionSensor::getSensorType() const { return "Motion"; }
+
+void MotionSensor::triggerMotion() { motionDetected_ = true; }
+void MotionSensor::clearMotion() { motionDetected_ = false; }
