a10c1f5ea1ef1f85f889011e92db8ac7d6d87e81
Sasha Doubik-Enman <sashadoubikenman@Sashas-MacBook-Air.local>
2026-03-12 10:19:21 +0300

    feat: add exception hierarchy

---
+++ include/sensors/sensor.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Sasha Doubik-Enman on 3/12/26.
+//
+
+#ifndef SMART_HOME_PROJECT_SENSOR_HPP
+#define SMART_HOME_PROJECT_SENSOR_HPP
+
+
+class sensor {
+};
+
+
+#endif //SMART_HOME_PROJECT_SENSOR_HPP
