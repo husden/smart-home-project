2b3fb3e35656518ead6acfdcf72bcb3b0704a841
tyagunova_daria <daria12112007@gmail.com>
2026-03-12 23:36:35 +0300

    test: add unit tests for ScheduledRule class

--- tests/rules/test_scheduled_rule.cpp
+++ tests/rules/test_scheduled_rule.cpp
@@ -1,1 +1,35 @@
+#include <gtest/gtest.h>
+#include "rules/scheduled_rule.hpp"
+#include <thread>
+#include <chrono>
 
+TEST(ScheduledRuleTest, ExecuteAtScheduledTime) {
+bool actionExecuted = false;
+
+auto now = std::chrono::system_clock::now();
+auto later = now + std::chrono::seconds(1);
+
+ScheduledRule rule("TestRule",
+                   later,
+                   [&actionExecuted]() { actionExecuted = true; },
+                   false
+);
+
+EXPECT_FALSE(rule.checkCondition());
+std::this_thread::sleep_for(std::chrono::seconds(2));
+EXPECT_TRUE(rule.checkCondition());
+
+rule.execute();
+EXPECT_TRUE(actionExecuted);
+}
+
+TEST(ScheduledRuleTest, MakeTimeWorks) {
+auto time = ScheduledRule::makeTime(14, 30);
+
+auto time_t = std::chrono::system_clock::to_time_t(time);
+std::tm tm = *std::localtime(&time_t);
+
+EXPECT_EQ(tm.tm_hour, 14);
+EXPECT_EQ(tm.tm_min, 30);
+EXPECT_EQ(tm.tm_sec, 0);
+}
