430490fc77c46dd56c745de7e89f03bcf80e5750
Sasha Doubik-Enman <sashadoubikenman@Sashas-MacBook-Air.local>
2026-03-12 10:23:42 +0300

    feat: add Sensor abstract class

--- include/sensors/sensor.hpp
+++ include/sensors/sensor.hpp
@@ -1,12 +1,29 @@
-//
-// Created by Sasha Doubik-Enman on 3/12/26.
-//
 
 #ifndef SMART_HOME_PROJECT_SENSOR_HPP
 #define SMART_HOME_PROJECT_SENSOR_HPP
 
+#include <string>
+#include <optional>
 
-class sensor {
+#include "../exceptions/exceptions.hpp"
+class Sensor {
+private:
+    std::string name_;
+    bool isOperational_;
+public:
+    virtual ~Sensor() = default;
+    virtual std::optional<double> readValue() = 0;
+    virtual std::string getSensorType() const = 0;
+    virtual std::string getName() const = 0;
+
+    bool isOperational() const { return isOperational_; }
+protected:
+    explicit Sensor(std::string name)
+    : name_(std::move(name)), isOperational_(true) {
+        if (name_.empty()) {
+            throw InvalidArgumentException("Sensor name cannot be empty");
+        }
+    }
 };
 
 
