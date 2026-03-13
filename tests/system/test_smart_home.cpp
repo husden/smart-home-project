361dc71433bf1c5f300b93ed82f916fb4dd2b237
tyagunova_daria <daria12112007@gmail.com>
2026-03-13 08:53:59 +0300

    test: add unit tests for SmartHomeSystem class

--- tests/system/test_smart_home.cpp
+++ tests/system/test_smart_home.cpp
@@ -1,1 +1,41 @@
+#include <gtest/gtest.h>
+#include "system/smart_home_system.hpp"
+#include "devices/light.hpp"
+#include "sensors/motion_sensor.hpp"
+#include "rules/condition_rule.hpp"
 
+TEST(SmartHomeSystemTest, AddAndFindDevice) {
+SmartHomeSystem home;
+
+auto light = std::make_unique<Light>("TestLight");
+home.addDevice(std::move(light));
+
+auto found = home.findDevice("TestLight");
+EXPECT_TRUE(found.has_value());
+
+auto notFound = home.findDevice("NonExistent");
+EXPECT_FALSE(notFound.has_value());
+}
+
+TEST(SmartHomeSystemTest, AddAndFindSensor) {
+SmartHomeSystem home;
+
+auto sensor = std::make_shared<MotionSensor>("TestSensor");
+home.addSensor(sensor);
+
+auto found = home.findSensor("TestSensor");
+EXPECT_TRUE(found.has_value());
+}
+
+TEST(SmartHomeSystemTest, AddRule) {
+SmartHomeSystem home;
+
+auto rule = std::make_unique<ConditionRule>(
+        "TestRule",
+        []() { return true; },
+        []() {}
+);
+home.addRule(std::move(rule));
+
+EXPECT_EQ(home.getRuleCount(), 1);
+}
